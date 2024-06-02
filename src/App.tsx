e8f2529621cd7f42c7d1fd98fea6c709550bc7a9
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-06-02 12:12:06 -0300

    add i18 provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./presentation/styled/themes";
 import { GlobalStyles } from "./presentation/styled/GlobalStyles";
 import Home from "./presentation/pages";
+import { I18nextProvider } from "react-i18next";
+import i18 from "~/infrastructure/config/i18";
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
@@ -12,8 +14,10 @@ function App() {
 
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-      <GlobalStyles />
-      <Home toggleTheme={toggleTheme} />
+      <I18nextProvider i18n={i18}>
+        <GlobalStyles />
+        <Home toggleTheme={toggleTheme} />
+      </I18nextProvider>
     </ThemeProvider>
   );
 }

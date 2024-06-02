505068ed71e51daf9f47104d2b89fca12d7b2f39
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-06-02 11:18:48 -0300

    add themes interface

---
+++ src/domain/interfaces/themes.d.ts
@@ -0,0 +1,24 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      background1: string;
+      background2: string;
+      text: string;
+      icon: string;
+      button: {
+        full: string;
+        outline: string;
+      };
+      hover: {
+        icon: string;
+        text: string;
+        button: {
+          full: string;
+          outline: string;
+        };
+      };
+    };
+  }
+}

3b93612a9f51927e031b40976e140531f35c85e9
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-07-11 23:27:28 -0300

    add icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as GithubIcon } from "./github.svg?react";
+export { default as GitlabIcon } from "./gitlab.svg?react";
+export { default as LinkedinIcon } from "./linkedin.svg?react";

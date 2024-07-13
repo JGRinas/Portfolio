0bb8f13deb6ee2b246252436b3c90e4f69284d0c
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-07-13 14:25:29 -0300

    add new icon and translations

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as GithubIcon } from "./github.svg?react";
 export { default as GitlabIcon } from "./gitlab.svg?react";
 export { default as LinkedinIcon } from "./linkedin.svg?react";
+export { default as ArrowBottom } from "./arrow-bottom.svg?react";

822eb0684dd7c9cc5b2ffe60543845578fb06028
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-05 10:18:42 -0300

    add new icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,23 @@ export { default as GithubIcon } from "./github.svg?react";
 export { default as GitlabIcon } from "./gitlab.svg?react";
 export { default as LinkedinIcon } from "./linkedin.svg?react";
 export { default as ArrowBottom } from "./arrow-bottom.svg?react";
+export { default as DiscordIcon } from "./discord.svg?react";
+export { default as EmailIcon } from "./email.svg?react";
+
+//Technologies
+export { default as DockerIcon } from "./technologies/docker.svg?react";
+export { default as ExpoIcon } from "./technologies/expo.svg?react";
+export { default as ExpressIcon } from "./technologies/express.svg?react";
+export { default as FigmaIcon } from "./technologies/figma.svg?react";
+export { default as GitIcon } from "./technologies/git.svg?react";
+export { default as JestIcon } from "./technologies/jest.svg?react";
+export { default as MongoIcon } from "./technologies/mongo.svg?react";
+export { default as MySqlIcon } from "./technologies/mysql.svg?react";
+export { default as NextIcon } from "./technologies/next.svg?react";
+export { default as NodeIcon } from "./technologies/node.svg?react";
+export { default as PhpIcon } from "./technologies/php.svg?react";
+export { default as PostmanIcon } from "./technologies/postman.svg?react";
+export { default as ReactIcon } from "./technologies/react.svg?react";
+export { default as SqlIcon } from "./technologies/sql.svg?react";
+export { default as TestingLibraryIcon } from "./technologies/testing-library.svg?react";
+export { default as VueIcon } from "./technologies/vue.svg?react";

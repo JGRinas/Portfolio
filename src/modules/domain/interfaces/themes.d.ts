30771279cd802ea828a964457ddc017f7633285f
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-05 16:08:20 -0300

    modified theme interfaces

--- src/modules/domain/interfaces/themes.d.ts
+++ src/modules/domain/interfaces/themes.d.ts
@@ -3,23 +3,11 @@ import "styled-components";
 declare module "styled-components" {
   export interface DefaultTheme {
     colors: {
-      background1: string;
-      background2: string;
-      text: string;
-      icon: string;
       primary: string;
       secondary: string;
-      button: {
-        full: string;
-        outline: string;
-      };
       hover: {
-        icon: string;
-        text: string;
-        button: {
-          full: string;
-          outline: string;
-        };
+        primary: string;
+        secondary: string;
       };
     };
   }

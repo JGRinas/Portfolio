a64544b186e7ed80228d033b2ee8e72f50bc07ac
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-05 10:34:42 -0300

    add font Monserrat

--- src/modules/presentation/styled/GlobalStyles.ts
+++ src/modules/presentation/styled/GlobalStyles.ts
@@ -2,25 +2,22 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    background: linear-gradient(
-      135deg,
-      ${(props) => props.theme.colors.background1} 0%,
-      ${(props) => props.theme.colors.background1} 85%,
-      ${(props) => props.theme.colors.background2} 100%
-    );
     color: ${(props) => props.theme.colors.text};
-    font-family: 'Roboto', sans-serif;
+    font-family: "Montserrat", sans-serif;
     transition: all 0.25s linear;
     min-height: 100vh;
     margin: 0;
-  }
-  pre {
-    font-family: 'Courier New', Courier, monospace;
   }
   .icon {
     fill: ${(props) => props.theme.colors.icon};
     &:hover {
       fill: ${(props) => props.theme.colors.hover.icon};
     }
+  }
+  h1, h2, h3 {
+    font-weight: 'bold';
+  }
+  p {
+    font-weight: 500;
   }
 `;

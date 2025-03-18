c4e14569faff994dc4effa89547a07b0570716a1
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2025-03-18 16:10:37 -0300

    feat: enhance global styles with reset and improved text rendering

--- src/modules/presentation/styled/GlobalStyles.ts
+++ src/modules/presentation/styled/GlobalStyles.ts
@@ -1,12 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
   body {
     color: ${(props) => props.theme.colors.primary};
     font-family: "Montserrat", sans-serif;
     transition: all 0.25s linear;
     min-height: 100vh;
     margin: 0 5rem;
+    -webkit-font-smoothing: antialiased; 
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    background: ${(props) => props.theme.colors.secondary};
     background-image: ${(props) => `url(${props.theme.background})`}; 
     background-position: center;
     background-repeat: repeat-y;
